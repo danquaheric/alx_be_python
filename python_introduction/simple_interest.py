9b496c780f960d259e25fcbbebe2443348365b44
Eric Danquah <danquah@aircellsolutions.com>
2025-11-09 11:54:21 +0000

    Add script to calculate simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+#define variables
+principal = 1000
+rate = 0.05
+time = 3
+
+#calculate simple interest
+interest = principal * rate * time
+
+#print result
+print(f"The simple interest is: {interest}")
