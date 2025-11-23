5d22b25c9e69d155ae14d0d6056dc73c7d5c32a4
Eric Danquah <danquah@aircellsolutions.com>
2025-11-23 15:39:43 +0000

    Added arithmetic_operations.py and main.py

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,17 @@
+from typing import Union
+
+def perform_operation(num1: float, num2: float, operation: str) -> Union[float, str]:
+    op = operation.strip().lower()
+
+    if op == "add":
+        return num1 + num2
+    elif op == "subtract":
+        return num1 - num2
+    elif op == "multiply":
+        return num1 * num2
+    elif op == "divide":
+        if num2 == 0:
+            return "Error: Division by zero"
+        return num1 / num2
+    else:
+        return "Error: invalid operation"
